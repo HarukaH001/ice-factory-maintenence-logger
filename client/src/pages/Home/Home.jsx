ffc0c9a85b5e86b27670649218a499bba675cb97
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2020-08-23 00:15:52 +0700

    update

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,29 +1,31 @@
 import React from 'react'
 import './Home.scss'
 // import { Link } from 'react-router-dom';
-import { Jumbotron, Button, Form } from 'react-bootstrap';
+import { Jumbotron, Button, Form } from 'react-bootstrap'
 
 export const Home = () => {
   return (
     <div className="container">
       <div className="home-container">
-        <h1 style={{fontStyle: "Bold"}}>ประวัติรายการ</h1>
+        <h2 style={{ fontStyle: "Bold" }}>ประวัติรายการ</h2>
         <Form.Group controlId="exampleForm.ControlInput1">
-          <Form.Control type="email" placeholder="&#xF002; ค้นหา..." />
+          <Form.Control type="email" placeholder="&#xf002; ค้นหา..." />
         </Form.Group>
-        {/* <Jumbotron>
-          <h1 style={{ color: "#007bff" }}>หน้าหลัก</h1>
-          <p>
-            Ice Factory Maintenance WebApp
+        <Button variant="primary" className="add-btn" >+ เพิ่มรายการ</Button>
+        <div className="card-wrapper">
+          <Jumbotron>
+            <h1 style={{ color: "#007bff" }}>หน้าหลัก</h1>
+            <p>
+              Ice Factory Maintenance WebApp
           </p>
-          <a href="https://github.com/HarukaH001/ice-factory-maintenence-logger">
-            <Button variant="primary" >GitHub Repository</Button>
-          </a>
-          <a href="/login">
-            <Button variant="secondary" style={{ marginLeft: "1rem" }}>ลงชื่อเข้าใช้</Button>
-          </a>
-        </Jumbotron> */}
-        <p style={{ position: "absolute", bottom: 0, color: "#B9B9B9" }}>WIP // React-Bootstrap v1.3.0</p>
+            <a href="https://github.com/HarukaH001/ice-factory-maintenence-logger">
+              <Button variant="primary" >GitHub Repository</Button>
+            </a>
+            <a href="/login">
+              <Button variant="secondary" style={{ marginLeft: "1rem" }}>ลงชื่อเข้าใช้</Button>
+            </a>
+          </Jumbotron>
+        </div>
       </div>
     </div>
   )
