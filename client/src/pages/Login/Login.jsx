5e30979b80103a34e4cbaa90a46c262ceca0ba82
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2020-08-23 18:45:48 +0700

    add Alert and update mockup

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import './Login.scss'
-import { Button, InputGroup, FormControl, Dropdown } from 'react-bootstrap'
+import { Button, InputGroup, FormControl, Dropdown, Alert } from 'react-bootstrap'
 import { FaLock } from 'react-icons/fa'
 import Service from '../../services/service.js'
 
@@ -12,13 +12,13 @@ export const Login = () => {
   const userList = [{
     username: "admin",
     password: "admin"
-  },{
+  }, {
     username: "user1",
     password: "1234561"
-  },{
+  }, {
     username: "user2",
     password: "1234562"
-  },{
+  }, {
     username: "user3",
     password: "1234563"
   },]
@@ -35,43 +35,44 @@ export const Login = () => {
   //   })
   // }
 
-  useEffect(()=>{
-    Service.getAuthen().getUserList().then((value)=>{
+  useEffect(() => {
+    Service.getAuthen().getUserList().then((value) => {
       console.log(value)
     })
     // console.log(Get.getNahee())
-  },[])
+  }, [])
 
   return (
-    <div className="container">
-      <div className="login-container">
-        <h1 className="title">ลงชื่อเข้าใช้</h1>
-        <Dropdown className="username">
-          <Dropdown.Toggle variant="light" id="dropdown-basic">
-            {user}
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            {
-              userList.map((ele, i) => <Dropdown.Item key={i} href="#" onClick={() => setUser(ele.username)}>{ele.username}</Dropdown.Item>)
-            }
-          </Dropdown.Menu>
-        </Dropdown>
-
-        <InputGroup className="password">
-          <InputGroup.Prepend>
-            <InputGroup.Text id="basic-addon1"><FaLock /></InputGroup.Text>
-          </InputGroup.Prepend>
-          <FormControl
-            type="password"
-            placeholder="รหัสผ่าน"
-            aria-label="รหัสผ่าน"
-            aria-describedby="basic-addon1"
-          />
-        </InputGroup>
-        {loginError ? <p style={{ color: "red", marginTop: "1vh", fontSize: "14px", textAlign: "end" }}>รหัสผ่านไม่ถูกต้อง</p> : null}
-        <Button variant="primary" className="login-btn" onClick={login}>ลงชื่อเข้าใช้</Button>
-        <a href="/" style={{ marginTop: "1vh", fontSize: "14px", textAlign: "center", width: "100%" }}>ไปหน้าหลัก</a>
-      </div>
-    </div >
+    <div className="Login">
+      <div className="container">
+        <div className="login-container">
+          <h1 className="title">ลงชื่อเข้าใช้</h1>
+          <Dropdown className="username">
+            <Dropdown.Toggle variant="light" id="dropdown-basic">
+              {user}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {
+                userList.map((ele, i) => <Dropdown.Item key={i} href="#" onClick={() => setUser(ele.username)}>{ele.username}</Dropdown.Item>)
+              }
+            </Dropdown.Menu>
+          </Dropdown>
+          <InputGroup className="password">
+            <InputGroup.Prepend>
+              <InputGroup.Text id="lock"><FaLock /></InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              type="password"
+              placeholder="รหัสผ่าน"
+              aria-label="รหัสผ่าน"
+              aria-describedby="password"
+            />
+          </InputGroup>
+          {loginError ? <Alert variant="danger" className="alert">รหัสผ่านไม่ถูกต้อง</Alert> : null}
+          <Button variant="primary" className="login-btn" onClick={login}>ลงชื่อเข้าใช้</Button>
+          <a href="/" style={{ marginTop: "1vh", fontSize: "14px", textAlign: "center", width: "100%" }}>ไปหน้าหลัก</a>
+        </div>
+      </div >
+    </div>
   )
 }
