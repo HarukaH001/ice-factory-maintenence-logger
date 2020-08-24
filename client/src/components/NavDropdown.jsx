98957b0edcebc84b0c265c5472c2ea758954bc22
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2020-08-25 00:40:35 +0700

    formatted

--- client/src/components/NavDropdown.jsx
+++ client/src/components/NavDropdown.jsx
@@ -4,32 +4,32 @@ import firebase, { Authen } from '../services/service.js'
 import { useHistory } from 'react-router-dom';
 
 export const NavDropdown = ({ data, _disabled }) => {
-    const [user, setUser] = useState('')
-    const history = useHistory()
+  const [user, setUser] = useState('')
+  const history = useHistory()
 
-    useEffect(()=>{
-        Authen.auth().onAuthStateChanged(user=>{
-            if(user){
-                Authen.getUser().then(res=>{
-                    setUser(res)
-                })
-            }
+  useEffect(() => {
+    Authen.auth().onAuthStateChanged(user => {
+      if (user) {
+        Authen.getUser().then(res => {
+          setUser(res)
         })
-    },[])
+      }
+    })
+  }, [])
 
-    return (
-        <>
-            <Dropdown alignRight>
-                <Dropdown.Toggle variant="light" id="dropdown-basic">&#xF0C9;</Dropdown.Toggle>
-                <Dropdown.Menu>
-                    {user?.role == 'admin' && <Dropdown.Item href="/" disabled={_disabled==="home"}>ประวัติการซ่อม</Dropdown.Item>}
-                    {user?.role == 'admin' && <Dropdown.Item href="/users" disabled={_disabled==="users"}>จัดการผู้ใช้</Dropdown.Item>}
-                    {user?.role == 'admin' && <Dropdown.Item href="/machines" disabled={_disabled==="machine"}>จัดการข้อมูลเครื่อง</Dropdown.Item>}
-                    {user?.role == 'admin' && <Dropdown.Divider />}
-                    <Dropdown.Item href="#" onClick={()=>Authen.logout()} style={{ color: "red" }}>ออกจากระบบ</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
-        </>
-    )
+  return (
+    <>
+      <Dropdown alignRight>
+        <Dropdown.Toggle variant="light" id="dropdown-basic">&#xF0C9;</Dropdown.Toggle>
+        <Dropdown.Menu>
+          {user?.role == 'admin' && <Dropdown.Item href="/" disabled={_disabled === "home"}>ประวัติการซ่อม</Dropdown.Item>}
+          {user?.role == 'admin' && <Dropdown.Item href="/users" disabled={_disabled === "users"}>จัดการผู้ใช้</Dropdown.Item>}
+          {user?.role == 'admin' && <Dropdown.Item href="/machines" disabled={_disabled === "machine"}>จัดการข้อมูลเครื่อง</Dropdown.Item>}
+          {user?.role == 'admin' && <Dropdown.Divider />}
+          <Dropdown.Item href="#" onClick={() => Authen.logout()} style={{ color: "red" }}>ออกจากระบบ</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </>
+  )
 }
-  
+
