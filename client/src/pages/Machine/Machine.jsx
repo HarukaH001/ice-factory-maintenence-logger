84715c27280ad8854508d391a6f03b30c3b8e279
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2020-09-07 23:19:24 +0700

    add Card

--- client/src/pages/Machine/Machine.jsx
+++ client/src/pages/Machine/Machine.jsx
@@ -5,7 +5,10 @@ import { Button, InputGroup, FormControl } from 'react-bootstrap'
 
 export const Machine = () => {
   const history = useHistory()
+  const { path, url } = useRouteMatch()
   const { machine } = useParams()
+
+  const locationList = ["ฝั่งมอเตอร์", "ฝั่งวาล์วดูด"]
 
   return (
     <div className="Machine">
@@ -14,10 +17,19 @@ export const Machine = () => {
           <Button variant="light" onClick={() => history.goBack()}>&#xF053;</Button>
           <h2>{machine}</h2>
         </div>
-        <Button variant="primary" className="add-location" >+ เพิ่มตำแหน่งซ่อม</Button>
-        <div className="card-container">
+        <Link to={`${url}/new`}><Button variant="primary" className="add-location" >+ เพิ่มตำแหน่งซ่อม</Button></Link>
+        <div className="cards-container">
+          {locationList.map((ele, i) => <Card key={i} location={ele} />)}
         </div>
       </div>
+    </div>
+  )
+}
+
+const Card = ({ location }) => {
+  return (
+    <div className="card-container">
+      <h5 style={{ margin: 0 }}>{location}</h5>
     </div>
   )
 }
