5b64326b6c3b92e574ba6024f2499101d45f9943
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2020-09-12 22:43:07 +0700

    update deleteHandler and add confirm modal

--- client/src/pages/Machine/Machine.jsx
+++ client/src/pages/Machine/Machine.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Machine.scss'
 import { Link, useHistory, useParams, useRouteMatch } from 'react-router-dom';
-import { Button } from 'react-bootstrap'
+import { Button, Modal } from 'react-bootstrap'
 import firebase, { Data, Authen } from '../../services/service'
 
 export const Machine = () => {
@@ -11,12 +11,15 @@ export const Machine = () => {
 
   const locationList = ["ฝั่งมอเตอร์", "ฝั่งวาล์วดูด"]
 
-  function deleteMachineHandler(e){
+  const [showModal, setShowModal] = useState(false)
+  const handleClose = () => { setShowModal(false) }
+  const handleShow = () => { setShowModal(true) }
+
+  function deleteMachineHandler(e) {
     e.preventDefault()
-    //ทำอะไรสักอย่างก่อนลบ
-    Data.deleteMachine(num,machine).then(()=>{
-      //ทำอะไรสักอย่างหลังสั่งลบ
-      history.goBack()
+    handleClose()
+    Data.deleteMachine(num, machine).then(() => {
+      history.push("/sites/" + num + "?deleted=" + machine)
     })
   }
 
@@ -25,14 +28,27 @@ export const Machine = () => {
       <div className="container">
         <div className="header">
           <Button variant="light" onClick={() => history.goBack()}>&#xF053;</Button>
-          <h2>{machine}
-          </h2><Button style={{backgroundColor:'red',border:'none'}} onClick={deleteMachineHandler}>ลบ</Button>
+          <h2>{machine}</h2>
+          <Button variant="outline-danger" onClick={handleShow}>ลบ</Button>
         </div>
         <Link to={`${url}/new`}><Button variant="primary" className="add-location" >+ เพิ่มตำแหน่งซ่อม</Button></Link>
         <div className="cards-container">
           {locationList.map((ele, i) => <Card key={i} location={ele} />)}
         </div>
       </div>
+      <Modal show={showModal} onHide={handleClose} centered style={{ fontFamily: "IBM Plex Sans Thai" }} backdrop="static" keyboard={false}>
+        <Modal.Header>
+          <Modal.Title style={{ width: "100%" }}>ลบ "{machine}"</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <strong style={{ color: "#dc3545", display: "inline" }}>ตำแหน่งซ่อม และรายการอะไหล่ที่เกี่ยวข้องจะถูกลบทั้งหมด</strong>{" "}
+          <p style={{ color: "#dc3545", display: "inline" }}>คุณแน่ใจหรือไม่ว่าต้องการลบ "{machine}" ?</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" type="button" onClick={handleClose}>ยกเลิก</Button>
+          <Button variant="danger" type="button" onClick={deleteMachineHandler}>ลบ</Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   )
 }
