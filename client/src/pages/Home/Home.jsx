9adefb7db92d7c568f706013929b1131a02e4560
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2020-12-08 00:20:22 +0700

    Search change from onInput to onClick&onSubmit

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,147 +1,171 @@
-import React, { useState, useEffect } from 'react'
-import './Home.scss'
-import { Link, useLocation, useHistory } from 'react-router-dom';
-import { Button, Form, Toast } from 'react-bootstrap'
-import { HistoryCard, NavDropdown } from '../../components'
-import { Authen, Data } from '../../services/service';
+import React, { useState, useEffect } from "react";
+import "./Home.scss";
+import { Link, useLocation, useHistory } from "react-router-dom";
+import { Button, Form, Toast, InputGroup } from "react-bootstrap";
+import { HistoryCard, NavDropdown } from "../../components";
+import { Authen, Data } from "../../services/service";
 
-import stringSimilarity from 'string-similarity'
+import stringSimilarity from "string-similarity";
 
 export const Home = () => {
-  const history = useHistory()
-  const [search, setSearch] = useState('')
-  const [log, setLog] = useState([])
-  const [user, setUser] = useState()
-  const [xcel, setXcel] = useState([])
+  const history = useHistory();
+  const [search, setSearch] = useState("");
+  const [log, setLog] = useState([]);
+  const [user, setUser] = useState();
+  const [xcel, setXcel] = useState([]);
+  const [searched, setSearched] = useState(false);
 
   function useQuery() {
     return new URLSearchParams(useLocation().search);
   }
-  const del = useQuery().get("deleted")
+  const del = useQuery().get("deleted");
 
-  const [showDeleteSuccess, setShowDeleteSuccess] = useState(false)
-  const toggleShowDeleteSuccess = () => setShowDeleteSuccess(!showDeleteSuccess)
+  const [showDeleteSuccess, setShowDeleteSuccess] = useState(false);
+  const toggleShowDeleteSuccess = () =>
+    setShowDeleteSuccess(!showDeleteSuccess);
 
   useEffect(() => {
-    Authen.getUser().then(value => {
-      setUser(value)
-    })
-  }, [])
+    Authen.getUser().then((value) => {
+      setUser(value);
+    });
+  }, []);
 
   useEffect(() => {
     if (user) {
-      Data.getRecordRef().on('value', snapshot => {
+      Data.getRecordRef().on("value", (snapshot) => {
         if (snapshot) {
           if (snapshot.val()) {
-            const Data = Object.entries(snapshot.val()).map(ele => {
-              ele[1].lid = ele[0]
-              return ele[1]
-            })
-            Data.sort((a, b) => new Date(b.date) - new Date(a.date))
-            setLog(Data)
-          } else setLog([])
+            const Data = Object.entries(snapshot.val()).map((ele) => {
+              ele[1].lid = ele[0];
+              return ele[1];
+            });
+            Data.sort((a, b) => new Date(b.date) - new Date(a.date));
+            setLog(Data);
+          } else setLog([]);
         }
-      })
+      });
 
       return () => {
-        Data.getRecordRef().off()
-      }
+        Data.getRecordRef().off();
+      };
     }
-  }, [user])
+  }, [user]);
 
   useEffect(() => {
-    if (del) toggleShowDeleteSuccess()
-  }, [del])
+    if (del) toggleShowDeleteSuccess();
+  }, [del]);
 
-  useEffect(()=>{
-    if(log){
-      setXcel(prepareData(preSearch(log)))
+  useEffect(() => {
+    if (log) {
+      setXcel(prepareData(preSearch(log)));
     }
-  },[log])
+  }, [log]);
 
-  function prepareData(data){
-    let procData = []
-    data.reverse()
-    if(data && data.length>0){
-      data.forEach((ele, i)=>{
-        let details = []
-        if(ele.part){
-          ele.part.forEach((data)=>{
-            if(data.status !== 'ปกติ'){
-              details.push(data.status+''+data.rid)
+  function prepareData(data) {
+    let procData = [];
+    data.reverse();
+    if (data && data.length > 0) {
+      data.forEach((ele, i) => {
+        let details = [];
+        if (ele.part) {
+          ele.part.forEach((data) => {
+            if (data.status !== "ปกติ") {
+              details.push(data.status + "" + data.rid);
             }
-          })
-          details = details.join("  , ")
+          });
+          details = details.join("  , ");
           procData.push({
             "#": ele.lid,
-            "ว/ด/ป": (ele.dd<10 ? '0'+ele.dd : ele.dd)+'/'+(ele.mm<10 ?'0'+ele.mm : ele.mm)+'/'+ele.yyyy,
-            "เวลา": (ele.hour<10 ? '0'+ele.hour : ele.hour)+':'+(ele.min<10 ? '0'+ele.min : ele.min)+' น.',
-            "บ่อ": ele.location,
-            "เครื่องจักร" : ele.machine,
-            "ตำแหน่ง" : ele.position,
-            "รายละเอียด":details,
-            "ผู้ซ่อม" : ele.technician,
-          })
+            "ว/ด/ป":
+              (ele.dd < 10 ? "0" + ele.dd : ele.dd) +
+              "/" +
+              (ele.mm < 10 ? "0" + ele.mm : ele.mm) +
+              "/" +
+              ele.yyyy,
+            เวลา:
+              (ele.hour < 10 ? "0" + ele.hour : ele.hour) +
+              ":" +
+              (ele.min < 10 ? "0" + ele.min : ele.min) +
+              " น.",
+            บ่อ: ele.location,
+            เครื่องจักร: ele.machine,
+            ตำแหน่ง: ele.position,
+            รายละเอียด: details,
+            ผู้ซ่อม: ele.technician,
+          });
         }
-      })
+      });
     }
-    return procData
+    return procData;
   }
 
-  function customSearch(search, presearch){
-    const keyword = search.split(' ');
-    const target = ["location","position","machine","technician","dd","mm","yyyy","lid"]
-    target.reverse()
+  function customSearch(search, presearch) {
+    const keyword = search.split(" ");
+    const target = [
+      "location",
+      "position",
+      "machine",
+      "technician",
+      "dd",
+      "mm",
+      "yyyy",
+      "lid",
+    ];
+    target.reverse();
 
-    return presearch.map(ele=>{
-      let percent = 0
-      target.forEach((tg,i)=>{
-        let weight = 0
-        keyword.forEach(kw=>{
-          if(ele[tg]){
-              const temp = ele[tg]+""
-              if(kw.length > 1){
-                weight += stringSimilarity.compareTwoStrings(kw.toLowerCase(), temp.toLowerCase())
-              }
-              else{
-                if(temp.toLowerCase().includes(kw.toLowerCase())){
-                  weight += 1/temp.length
+    return presearch
+      .map((ele) => {
+        let percent = 0;
+        target.forEach((tg, i) => {
+          let weight = 0;
+          keyword.forEach((kw) => {
+            if (ele[tg]) {
+              const temp = ele[tg] + "";
+              if (kw.length > 1) {
+                weight += stringSimilarity.compareTwoStrings(
+                  kw.toLowerCase(),
+                  temp.toLowerCase()
+                );
+              } else {
+                if (temp.toLowerCase().includes(kw.toLowerCase())) {
+                  weight += 1 / temp.length;
                 }
               }
             }
-        })
-        percent += weight*(i+1)
+          });
+          percent += weight * (i + 1);
+        });
+        ele.percent = percent;
+        return ele;
       })
-      ele.percent = percent
-      return ele
-    }).sort((a,b)=>{
-      return b.percent - a.percent
-    })
+      .sort((a, b) => {
+        return b.percent - a.percent;
+      });
   }
 
-  function preSearch(){
-    return log.map(ele=>{
-      const date = new Date(ele.date)
-        ele.dd = date.getDate()
-        ele.mm = date.getMonth()+ 1
-        ele.yyyy = date.getFullYear()
-        ele.hour = date.getHours()
-        ele.min = date.getMinutes()
-      return ele
-    })
+  function preSearch() {
+    return log.map((ele) => {
+      const date = new Date(ele.date);
+      ele.dd = date.getDate();
+      ele.mm = date.getMonth() + 1;
+      ele.yyyy = date.getFullYear();
+      ele.hour = date.getHours();
+      ele.min = date.getMinutes();
+      return ele;
+    });
   }
 
   function cardRender(search) {
-    const output = customSearch(search, preSearch(log))
+    const output = customSearch(search, preSearch(log));
 
-    if(search){
-      return output.map((ele,i)=>{
-        return (<HistoryCard user={user} data={ele} key={i} />)
-      })
+    if (search) {
+      return output.map((ele, i) => {
+        return <HistoryCard user={user} data={ele} key={i} />;
+      });
     } else {
-      return log.map((ele,i)=>{
-        return (<HistoryCard user={user} data={ele} key={i} />)
-      })
+      return log.map((ele, i) => {
+        return <HistoryCard user={user} data={ele} key={i} />;
+      });
     }
   }
 
@@ -153,28 +177,61 @@ export const Home = () => {
             <h2 style={{ fontStyle: "Bold" }}>การซ่อมบำรุง</h2>
             <NavDropdown _disabled={"home"} data={xcel}></NavDropdown>
           </div>
-          <Form.Group controlId="exampleForm.ControlInput1">
-            <Form.Control type="text" placeholder="&#xF002;  ค้นหารายการ" onInput={(e) => setSearch(e.target.value)} />
-          </Form.Group>
-          <Link to="/add"><Button variant="primary" className="add-btn">+ เพิ่มรายการซ่อม</Button></Link>
+          <Form className="search-form" onSubmit={(e) => {e.preventDefault(); setSearched(true)}}>
+            <InputGroup className="search-btn">
+              <Form.Control
+                type="text"
+                placeholder="ค้นหารายการซ่อม"
+                onInput={(e) => {
+                  setSearched(false);
+                  setSearch(e.target.value);
+                }}
+              />
+              <InputGroup.Append>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  onClick={() => setSearched(true)}
+                  title="ค้นหา"
+                >
+                  &#xF002;
+                </Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Form>
+          <Link to="/add">
+            <Button variant="primary" className="add-btn">
+              + เพิ่มรายการซ่อม
+            </Button>
+          </Link>
           <div className="card-wrapper">
-            {cardRender(search)}
+            {cardRender(searched ? search : "")}
           </div>
         </div>
       </div>
-      <Toast show={showDeleteSuccess} onClose={() => { toggleShowDeleteSuccess(); history.replace("") }} delay={3500} autohide
+      <Toast
+        show={showDeleteSuccess}
+        onClose={() => {
+          toggleShowDeleteSuccess();
+          history.replace("");
+        }}
+        delay={3500}
+        autohide
         style={{
-          position: 'fixed',
-          bottom: '2rem',
-          left: '50%',
-          backgroundColor: '#28a745',
-          color: 'white',
-          transform: 'translateX(-50%)',
+          position: "fixed",
+          bottom: "2rem",
+          left: "50%",
+          backgroundColor: "#28a745",
+          color: "white",
+          transform: "translateX(-50%)",
           width: "100%",
-          textAlign: "center"
-        }}>
-        <Toast.Body><b style={{ fontSize: "18px" }}>ลบรายการซ่อม #{del} เรียบร้อยแล้ว</b></Toast.Body>
+          textAlign: "center",
+        }}
+      >
+        <Toast.Body>
+          <b style={{ fontSize: "18px" }}>ลบรายการซ่อม #{del} เรียบร้อยแล้ว</b>
+        </Toast.Body>
       </Toast>
     </div>
-  )
-}
+  );
+};
